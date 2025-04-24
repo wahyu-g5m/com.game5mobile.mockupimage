84d201b80a64cc337180c551424ef18136f71561
wahyugame5mobile <wahyu@game5mobile.com>
2025-04-24 17:02:38 +0700

    init: initialize package

---
+++ Editor/MockupImageEditor.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CircleSort
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(MockupImage))]
+    public class MockupImageEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUILayout.Space(10);
+
+            var mockupImage = (MockupImage)target;
+
+            if (GUILayout.Button("Opaque"))
+            {
+                mockupImage.SetOpaque();
+                EditorUtility.SetDirty(mockupImage); // Mark as dirty to force Unity to refresh the inspector
+                SceneView.RepaintAll();
+                Undo.RecordObject(mockupImage, "Change Opacity");
+            }
+
+            GUILayout.Space(5);
+
+            if (GUILayout.Button("Transparent"))
+            {
+                mockupImage.SetTransparent();
+                EditorUtility.SetDirty(mockupImage); // Mark as dirty to force Unity to refresh the inspector
+                SceneView.RepaintAll();
+                Undo.RecordObject(mockupImage, "Change Opacity");
+            }
+        }
+    }
+#endif
+}
---
+++ Runtime/MockupImage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CircleSort
+{
+    public class MockupImage : MonoBehaviour
+    {
+        [SerializeField] private Image image;
+
+        public void SetOpaque()
+        {
+            Color color = image.color;
+            color.a = 1f; // Set the alpha value to 1 (opaque)
+            image.color = color;
+        }
+
+        public void SetTransparent()
+        {
+            Color color = image.color;
+            color.a = 0.3f;
+            image.color = color;
+        }
+    }
+}
